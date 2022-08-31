d927d01e8da6aaa6ae4f1fa3161fe6049e14f411
emmyhuynh <emmy.huynh8@gmail.com>
2022-08-31 14:40:10 -0700

    added music and instrument classes

---
+++ MyBand/Instrument.swift
@@ -0,0 +1,24 @@
+//
+//  Instrument.swift
+//  MyBand
+//
+//  Created by Emmy Huynh on 8/31/22.
+//
+
+import Foundation
+
+class Music {
+    let notes: [String]
+    
+    init(notes: [String]) {
+        self.notes = notes
+    }
+}
+
+class Instrument {
+    let brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+}
